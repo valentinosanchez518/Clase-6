7a74613fe10213f52db627e512b69fd10f5d7911
valentinosanchez518 <141331384+valentinosanchez518@users.noreply.github.com>
2024-09-06 09:43:57 -0300

    Initial commit

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform targetTR;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = targetTR.position;
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed",velocity);
+    }
+}
---
+++ Assets/charachterAgent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class charachterAgent : MonoBehaviour
+{
+    public NavMeshAgent agente;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.destination = target.position;
+    }
+}
